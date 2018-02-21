3458b1b2182c2366fc4544336270191693182197
atr <atr@ciklum.com>
2018-02-22 01:55:56 +0300

    Coins exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    var coinsArr   = {};
+    var coins      = [50, 25, 10, 5, 1];
+    var coinsNames = ["H", "Q", "D", "N", "P"];
+
+    if (currency <= 0) {
+        return {};
+    }
+
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+
+    getCoinsCount(currency, 0);
+
+    function getCoinsCount(amount, number) {
+        var coin = parseInt(amount/coins[number]);
+        var left = (amount%coins[number]);
+
+        if (coin) {
+            coinsArr[coinsNames[number]] = coin;
+            amount = amount-coin*coins[number];
+        }
+
+        if (left) {
+            getCoinsCount(amount, number+1)
+        } else {
+            return;
+        }
+    }
+
+    return coinsArr;
+};
